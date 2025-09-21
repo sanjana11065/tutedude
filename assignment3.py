5e97d8e75f8548d12bba6f7c80c31cf13cf0126c
sanjana11065 <sanjanareddymattamadugu@gmail.com>
2025-09-21 08:30:52 +0530

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,23 @@
+#task 1
+'''def factorial(n):
+    if n<2:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+
+n=int(input("enter a number"))
+ans=factorial(n)
+print(ans)'''
+
+
+#task 2
+
+import math
+num=int(input("enter a number"))
+square_root = math.sqrt(num)
+logarithm = math.log(num)
+sine = math.sin(num)
+print("the square root of num is",square_root)
+print("the logarithm of num is",logarithm)
+print("the sine of num is",sine)
